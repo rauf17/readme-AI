24f0482fd7b9e0ca08a5f47b9f659db0585d1fd4
connect2rauf17@gmail.com <connect2rauf17@gmail.com>
2026-04-30 01:24:17 +0500

    Integrated Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const geistMono = JetBrains_Mono({ subsets: ["latin"], variable: "--font-geist-mono" });
@@ -20,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${geistMono.variable}`}>{children}</body>
+      <body className={`${inter.variable} ${geistMono.variable}`}>{children} <Analytics /> </body>
     </html>
   );
 }
